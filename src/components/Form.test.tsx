30e7cd50ef9cf636d90825e5a2ff17c42c3024a4
saiful_bashar <robal.bashar59@gmail.com>
2021-10-27 13:41:34 +0600

    fix test case

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -7,7 +7,7 @@ test("Rendering and submitting a Form component", async () => {
   const handleSubmit = jest.fn();
   render(<ExpenseForm onAddTransaction={handleSubmit} />);
 
-  userEvent.type(screen.getByLabelText(/text/i), "Book");
+  userEvent.type(screen.getByLabelText(/transaction name/i), "Book");
   userEvent.type(screen.getByLabelText(/amount/i), "+51");
 
   userEvent.click(screen.getByRole("button", { name: /Add transaction/i }));
